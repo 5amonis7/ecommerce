3bdf3a67b451162b2af8213d1617ec83024f64e3
Andrew Monis <5andrewmonis7@gmail.com>
2022-01-31 13:42:56 -0500

    updated Navbar

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -9,7 +9,15 @@ const Nav = (props) => {
   
 
   return <div className='nav'>
-            <NavLink to='/ecommerce' className='nav-brand'>Frontier</NavLink>
+            <NavLink to='/ecommerce' id='nav-brand'>Frontier</NavLink>
+
+            <div className='nav-bar'>
+              <NavLink className='nav-bar_link' to='/ecommerce'>Home</NavLink>
+              <NavLink className='nav-bar_link' to='/school'>School</NavLink>
+              <NavLink className='nav-bar_link' to='/hiking'>Hiking</NavLink>
+              <NavLink className='nav-bar_link' to='/work'>Work</NavLink>
+            </div>
+
             <Hamburger state={props.state} onClick={props.onClick} />
             <NavLink to='/cart' className='cart-btn'><span>{props.cartAmount == 0 ? '': props.cartAmount}</span><FontAwesomeIcon icon={faCartPlus} /></NavLink>
         </div>;
